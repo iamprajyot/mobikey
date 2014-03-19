953b35f2ece4cff8188dba89e0bafc703fae08f1
iamprajyot <iamprajyot@github.com>
2014-03-19 12:21:14 +0530

    On continous mouse down event added

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 var prev_button = null;
 var prev_time = 0;
 var click_count = 0;
-
+var first_time = 0;
+var second_time = 0;
 var arr = [
     ['1', '.', ',', '!'],
     ['2', 'a', 'b', 'c'],
@@ -14,33 +15,54 @@ var arr = [
     ['9', 'w', 'x', 'y', 'z']
 ];
 
-
-
 jQuery(document).ready(function() {
-    jQuery("#phone").find("button").click(function(event) {
+    jQuery("#phone").find("button").mousedown(function(event) {
+        first_time = (new Date()).getTime();
         var button_pressed = jQuery(event.currentTarget).data("value");
-        var current_time = (new Date()).getTime();
-        if (isNaN(button_pressed) || button_pressed === 0)
-        {
+        timeout = setInterval(function() {
             jQuery("#result").val(t9(jQuery("#result").val(), button_pressed));
-        }
-        else {
-            if (button_pressed === prev_button)
-            {
-                click_count++;
-                sameButtonClick(prev_time, current_time, button_pressed);
-            }
-            else
-            {
-                click_count = 1;
-                jQuery("#result").val(t9(jQuery("#result").val(), arr[button_pressed - 1][1]));
-            }
-        }
+        }, 900);
+        return false;
+    });
 
-        prev_button = button_pressed;
-        prev_time = current_time;
+    jQuery("#phone").find("button").mouseup(function(event) {
+        second_time = (new Date()).getTime();
+        var button_pressed = jQuery(event.currentTarget).data("value");
+        clearInterval(timeout);
+        if (second_time - first_time < 900)
+        {
+            clickEvent(button_pressed);
+        }
+        return false;
+
     });
 });
+
+
+
+function clickEvent(button_pressed) {
+    var current_time = (new Date()).getTime();
+    if (isNaN(button_pressed) || button_pressed === 0)
+    {
+        jQuery("#result").val(t9(jQuery("#result").val(), button_pressed));
+    }
+    else {
+        if (button_pressed === prev_button)
+        {
+            click_count++;
+            sameButtonClick(prev_time, current_time, button_pressed);
+        }
+        else
+        {
+            click_count = 1;
+            jQuery("#result").val(t9(jQuery("#result").val(), arr[button_pressed - 1][1]));
+        }
+    }
+
+    prev_button = button_pressed;
+    prev_time = current_time;
+}
+
 function t9(text, button_pressed) {
     // Write your code here
     text = text + button_pressed;
@@ -48,7 +70,7 @@ function t9(text, button_pressed) {
 }
 
 function earlySameButton(text, button_pressed) {
-    // Write your code here
+// Write your code here
     text = text.toString().slice(0, -1) + button_pressed;
     return text;
 }
@@ -57,13 +79,13 @@ function sameButtonClick(prevTime, currentTime, buttonValue)
 {
     if (currentTime > (prevTime + 1000))
     {
-        // repeat value
+// repeat value
         click_count = 1;
         jQuery("#result").val(t9(jQuery("#result").val(), arr[buttonValue - 1][1]));
     }
     else
     {
-        // next value
+// next value
         if (click_count > (arr[buttonValue - 1].length - 1))
         {
             click_count = 1;
